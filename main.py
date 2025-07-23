c676b0ad1d0b7e2e3c41700c96dc1863f1659cb5
IrvinSivya <122709176+IrvinSivya@users.noreply.github.com>
2025-07-23 14:24:09 -0400

    updates site with accomplishments page, hero section, and header

---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from db_client import (
+    db,
+    skills_collection,
+    accomplishments_collection,
+    projects_collection,
+    extra_curriculars_collection,
+    experiences_collection
+)
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/accomplishments")
+def accomplishments():
+    accomplishments = list(accomplishments_collection.find())
+    return render_template("accomplishments.html", accomplishments=accomplishments)
+
+@app.route("/projects")
+def projects():
+    projects = list(projects_collection.find())
+    return render_template("projects.html",projects=projects)
+
+@app.route("/extra_curriculars")
+def extra_curriculars():
+    extra_curriculars = list(extra_curriculars_collection.find())
+    return render_template("extra_curriculars.html", extra_curriculars=extra_curriculars)
+
+@app.route("/experiences")
+def experiences():
+    experiences = list(experiences_collection.find())
+    return render_template("experience.html",experiences=experiences)
+
+def update_collection(collection_name, filter_query, update_fields):
+    collections = {'skills': skills_collection,
+                   'accomplishments': accomplishments_collection,
+                   'projects': projects_collection, 
+                   'extra_curriculars': extra_curriculars_collection,
+                   'experiences': experiences_collection}
+    
+    if collection_name not in collections:
+        raise ValueError("Collection don't exist")
+
+    collection = db[collection_name]
+    collection.update_one(filter_query, {"$set": update_fields}, upsert=False)
+
+def insert_document(collection_name, document_data):
+    collections = {'skills': skills_collection,
+                   'accomplishments': accomplishments_collection,
+                   'projects': projects_collection, 
+                   'extra_curriculars': extra_curriculars_collection,
+                   'experiences': experiences_collection}
+                   
+    if collection_name not in collections:
+        raise ValueError("Collection don't exist")
+
+    collection = db[collection_name]
+
+    if collection.find_one({"title": document_data["title"]}) is not None:
+        raise ValueError("Document with this title already exists")
+    
+    collection.insert_one(document_data)
+
+if __name__ == "__main__":
+    
+    #Examples:
+
+    '''
+    update_collection(collection_name='accomplishments', filter_query={'title': 'Dean\'s List Winner at Durham'}, 
+                      update_fields= {'description': 'After a long season of hard work and leadership, I was able to achieve the Dean\'s List at Durham College in 2024 with FRC team 1285!', 
+                      'image': 'durham_deans.jpg'})
+    '''
+    '''
+    insert_document(
+        collection_name='accomplishments',
+        document_data={
+            'title': 'Dean\'s List Winner at Durham',
+            'description': 'Dedicated effort and leadership throughout the season won me the Dean\'s List Award at Durham College in 2024 with FRC team 1285.',
+            'image': 'durham_deans.jpg'
+        }
+    )
+    '''
+    
+    app.run(debug=True)
